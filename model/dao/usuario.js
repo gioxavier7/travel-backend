51c460f0ea94c632f0776f594176141b73603625
GIOVANNA SOARES XAVIER <24122285@CFP127ALUNO.LOCAL>
2025-05-15 16:35:44 -0300

    tabela usuario

--- model/dao/usuario.js
+++ model/dao/usuario.js
@@ -16,23 +16,23 @@ const insertUsuario = async function(usuario){
     try {
         let sql = `insert into tbl_usuario (
                                             nome,
-                                            email,
                                             username,
-                                            foto_perfil,
-                                            biografia,
+                                            email,
                                             senha,
                                             palavra_chave,
-                                            data_conta
+                                            biografia,
+                                            data_conta,
+                                            foto_perfil
                                         )
                                     values (
                                             '${usuario.nome}',
+                                            '${usuario.username}',
                                             '${usuario.email}',
-                                            '${usuario.foto_perfil}',
-                                            '${usuario.biografia}',
                                             '${usuario.senha}',
                                             '${usuario.palavra_chave}',
+                                            '${usuario.biografia}',
                                             '${usuario.data_conta}',
-                                            '${usuario.username}'
+                                            '${usuario.foto_perfil}'
                                             )`
 
         //executa o script sql do banco de dados e aguarda o retorno 
@@ -49,4 +49,89 @@ const insertUsuario = async function(usuario){
 }
 
 //função pra atualizar usuário
-co
+const updateUsuario = async function(usuario){
+    try {
+        let sql = `update into tbl_usuario set nome = '${usuario.nome}',
+                                                username = '${usuario.username}',
+                                                email = '${usuario.email}',
+                                                senha = '${usuario.senha}',
+                                                palavra_chave = '${usuario.palavra_chave}',
+                                                biografia = '${usuario.biografia}',
+                                                data_conta = '${usuario.data_conta}',
+                                                foto_perfil = '${usuario.foto_perfil}'
+                                            where id=${usuario.id}`
+
+        let result = await prisma.$executeRawUnsafe(sql)
+
+        if(result)
+            return true
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+//função para excluir uma usuário existente no banco de dados
+const deleteUsuario = async function(id){
+    try {
+
+        let sql = 'delete from tbl_usuario where id='+id
+
+        let result = await prisma.$executeRawUnsafe(sql)
+
+        if(result)
+            return true
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+//função para retornar todos os usuarios do banco de dados
+const selectAllUsuario = async function(){
+    try {
+        let sql = 'select * from tbl_usuario order by id desc'
+
+        //executa o script sql no db e aguarda o retorno dos dados
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(result)
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+//função para listar um usuário pelo ID no banco de dados
+const selectByIdUsuario = async function(id){
+    try {
+        //script sql
+        let sql = 'select * from tbl_usuario where id='+id
+
+        //executa o script
+        let result = await prisma.$queryRawUnsafe(sql)
+
+        if(result)
+            return result
+        else
+            return false
+
+    } catch (error) {
+        return false
+    }
+}
+
+module.exports = {
+    insertUsuario,
+    updateUsuario,
+    deleteUsuario,
+    selectAllUsuario,
+    selectByIdUsuario
+}
